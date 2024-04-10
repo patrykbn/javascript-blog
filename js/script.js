f60df8750b68c5dd247b744b50d2e039612fe3d8
Patryk Bagan <patrykbn1997@gmail.com>
2024-04-10 21:26:08 +0200

    5.3 cwiczenie 2

--- js/script.js
+++ js/script.js
@@ -8,9 +8,17 @@ const titleClickHandler = function(){
     console.log('Link was clicked!');
     console.log(Event)
     /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
     /* add class 'active' to the clicked link */
+    const activeArticles = document.querySelectorAll('.posts .active');
 
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
     /* remove class 'active' from all articles */
 
     /* get 'href' attribute from the clicked link */
